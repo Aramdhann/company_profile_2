1bc56b9f7afe7c504f9dd68787e287f7bd4929f2
Aramdhann <radityamochamad@gmail.com>
2023-10-21 10:30:59 +0700

    updated navbar mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const navbar = () => {
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-64"
+              className="menu menu-sm dropdown-content mt-10 z-[1] p-2 shadow bg-base-100 rounded-box w-64"
             >
               <li>
                 <Link
