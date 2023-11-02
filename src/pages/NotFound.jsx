b518c8e4f67f809cbfc0885e0b5d9987f44ce0fd
Aramdhann <radityamochamad@gmail.com>
2023-11-02 15:12:00 +0700

    added 404 page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo_cashcepat from "../assets/logo_cashcepat.svg";
+import notFound from '../assets/404_cat.svg'
+
+const NotFound = () => {
+  return (
+    <>
+      <div className="flex h-screen items-center flex-col gap-5">
+        <img src={logo_cashcepat} alt="logo cashcepat" className="mt-20" />
+        <img src={notFound} alt="not found image" className="w-1/4"/>
+        <Link to="/">
+          <div className="btn rounded-full bg-primary hover:bg-secondary capitalize text-base font-bold border-none">
+            Go Home
+          </div>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default NotFound;
